d927735926aa2ab5ba5a8434e388a523e8cd0aa8
alibarkook <alibarkook@gmail.com>
2023-12-20 13:06:39 +0330

    implement user card

---
+++ angular-17/src/shared/components/user-card/user-card.component.ts
@@ -0,0 +1,48 @@
+import { Component, Input, SimpleChanges } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { IUserInfo } from '../../interfaces/user.interface';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+@Component({
+  selector: 'app-user-card',
+  standalone: true,
+  imports: [CommonModule, MatCardModule, HttpClientModule, MatButtonModule, MatProgressSpinnerModule],
+  templateUrl: './user-card.component.html',
+  styleUrl: './user-card.component.scss'
+})
+export class UserCardComponent {
+
+  @Input() username: string;
+
+  userInfo: IUserInfo;
+  loading: boolean;
+
+  
+  constructor(
+    private http: HttpClient
+  ){
+    console.log('user card component inited');
+  }
+  
+  
+  ngOnChanges(changes: SimpleChanges): void {
+    this.getUserInfo(this.username);
+  }
+
+
+  getUserInfo(username: string) {
+    this.loading = true;
+    
+    setTimeout(() => {
+      this.http.get<IUserInfo>(`https://api.github.com/users/${username}`).subscribe(res => {
+        this.loading = false;
+        this.userInfo = res;
+      })
+    }, 3000);
+
+  }
+
+}
